d2fb4482a94ddd102e269e83dfe2212e50737751
Lucas <lmaul77@gmail.com>
2022-07-29 13:57:44 -0500

    started working css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = ({loggedIn}) => {
     return (
-        <header>
+        <header id="HomeHeader">
             <h1>
             Fitness Tracker
             </h1>
