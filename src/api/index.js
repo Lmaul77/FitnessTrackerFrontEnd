324c1d9437752056c52bbde1fa3e0598d5742a39
Garrett Truong <aznkungfubunnee@gmail.com>
2022-08-02 12:55:15 -0500

    routines displayed

--- src/api/index.js
+++ src/api/index.js
@@ -1,92 +1,86 @@
-const BASE_URL = "http://fitnesstrac-kr.herokuapp.com/api"
+const BASE_URL = "http://fitnesstrac-kr.herokuapp.com/api";
+
+export async function registerUser(registerUsername, registerPassword) {
+  const response = await fetch(`${BASE_URL}/users/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: registerUsername,
+      password: registerPassword,
+    }),
+  });
+  const result = await response.json();
+  return result;
+}
+
+export async function loginUser(Username, Password) {
+  console.log(Username, Password);
+  const response = await fetch(`${BASE_URL}/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: Username,
+      password: Password,
+    }),
+  });
+  const result = await response.json();
+
+  return result.token;
+}
+
+export async function getUserInfo(token) {
+  try {
+    const response = await fetch(`${BASE_URL}/users/me`, {
+      headers: {
+        "Content-Type": "application/JSON",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getRoutines(){
+
+    const response = await fetch('http://fitnesstrac-kr.herokuapp.com/api/routines', {
+      headers: {
+        'Content-Type': 'application/json',
+      },})
+    const result = await response.json()
+    return result
+  }
 
 
-export async function registerUser(registerUsername, registerPassword){
-      console.log(registerUsername, registerPassword, "This is line 6 API INDEX")
-      const response = await 
-          fetch(`${BASE_URL}/users/register`,
-          {
-          method: "POST",
-          headers: {
-              'Content-Type': 'application/json'
-            },
-          body: JSON.stringify({
-             
-                username: registerUsername,
-                password: registerPassword
-              
-            })
-          }
-          )
-          const result = await response.json();
-          console.log( result, "This is line 22 API INDEX")
-
-          
-          return result
-      } 
-
-  export async function loginUser(Username, Password){
-      console.log(Username, Password)
-      const response = await 
-          fetch(`${BASE_URL}/users/login`,
-          {
-          method: "POST",
-          headers: {
-              'Content-Type': 'application/json'
-            },
-          body: JSON.stringify({
-          
-                username: Username,
-                password: Password
-              
-            })
-          }
-          )
-          const result = await response.json();
-  
-          return result.token}
-   
-  // export async function getUserInfo(token){
-  //   try { 
-  //     const response = await fetch(`${BASE_URL}/users/me`, {
-  
-  //     headers: {
-  //       'Content-Type': "application/JSON",
-  //       'Authorization': `Bearer ${token}`
-  //     }})
-      
-  //     const result = await response.json()
-  //     return result;
-  //   } catch (error) {
-  //     throw error;
-  //   }
-  // }
-  
-  
-//   export async function makeNewPost(title, description, price, location){
-//     const token = localStorage.getItem("token")
-//     const response = await fetch(`${BASE_URL}/posts`, {
-//       method: "POST",
-//       headers: {
-//         'Content-Type': 'application/json',
-//         'Authorization': `Bearer ${token}`
+// export async function makeNewPost(title, description, price, location) {
+//   const token = localStorage.getItem("token");
+//   const response = await fetch(`${BASE_URL}/posts`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${token}`,
+//     },
+//     body: JSON.stringify({
+//       post: {
+//         title: title,
+//         description: description,
+//         price: price,
+//         location: location,
 //       },
-//       body: JSON.stringify({
-//         post: {
-//           title: title,
-//           description: description,
-//           price: price,
-//           location: location
-//         }
-//     })
-//   })
-//     console.log(response, "response from makeNewPost")
-//     const result = await response.json()
-//     return result
-    
-//   }
-  
-  
+//     }),
+//   });
+//   console.log(response, "response from makeNewPost");
+//   const result = await response.json();
+//   return result;
+// }
+
 //   export async function editPost(
 //     title,
 //     description,
@@ -96,9 +90,9 @@ export async function registerUser(registerUsername, registerPassword){
 //     POST_ID
 //   ) {
 //     const token = getToken();
-  
+
 //     try {
-//       const response = await fetch(`${BASE_URL}/users/me`, 
+//       const response = await fetch(`${BASE_URL}/users/me`,
 //         {
 //           post: {
 //             title,
@@ -122,8 +116,7 @@ export async function registerUser(registerUsername, registerPassword){
 //       throw error;
 //     }
 //   }
-  
-  
+
 //   export const deletePost = async (token, POST_ID) => {
 //     try {
 //       await fetch(`${BASE_URL}/posts/${POST_ID}`, {
@@ -137,18 +130,3 @@ export async function registerUser(registerUsername, registerPassword){
 //       console.error(error);
 //     }
 //   };
-     
-//   export async function getAllPosts() {
-//           try {
-//               const response = await fetch(`${BASE_URL}/posts`)
-//               const result = await response.json()
-//               const posts = result.data.posts
-//               console.log(posts)
-//               return posts
-//           }
-//           catch (error) {
-//               console.error(error);
-  
-//          }
-//   }
-  
